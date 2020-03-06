2f9c0a2400ac0cf816fbd2acf58171639cfed4af
roman.kushyn <roman.kushyn@syndicode.com>
2020-03-07 00:46:04 +0200

    Fix parse url

--- src/lib/parseURL.js
+++ src/lib/parseURL.js
@@ -5,11 +5,12 @@ exports.parseURL = (url = process.env.DATABASE_URL) => {
     throw new Error(`Database url ${url} does not seem to be postgres`)
 
   let [host, port] = dbUrl.host.split(':')
+  const [user, password] = dbUrl.auth.slice(':')
   return {
     host: host,
     port: port || 5432,
     database: dbUrl.path.slice(1),
-    user: dbUrl.auth.substr(0, dbUrl.auth.indexOf(':')),
-    password: dbUrl.auth.substr(dbUrl.auth.indexOf(':') + 1, dbUrl.auth.length),
+    user,
+    password,
   }
 }
