ea91728eb0592912f6f6d0e631911a37976b7deb
roman.kushyn <roman.kushyn@syndicode.com>
2020-03-07 00:43:09 +0200

    Fix parse url

--- src/lib/parseURL.js
+++ src/lib/parseURL.js
@@ -8,7 +8,7 @@ exports.parseURL = (url = process.env.DATABASE_URL) => {
   return {
     host: host,
     port: port || 5432,
-    database: dbUrl.path,
+    database: dbUrl.path.slice(1),
     user: dbUrl.auth.substr(0, dbUrl.auth.indexOf(':')),
     password: dbUrl.auth.substr(dbUrl.auth.indexOf(':') + 1, dbUrl.auth.length),
   }
