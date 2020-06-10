d4ea32f20b0e63960ebebf059a3e47462a7d8f71
Roman <romadzao@gmail.com>
2020-06-10 08:14:29 +0300

    Export Prepared

--- src/adapter.d.ts
+++ src/adapter.d.ts
@@ -1,10 +1,10 @@
-import { AdapterProps, ConnectionSettingType, PgError, Log, ResultMode } from './types';
+import { AdapterProps, ConnectionSettingType, PgError, Log, ResultMode, Prepared } from './types';
 import { AdapterBase } from './lib/adapterBase';
 import { Transaction } from './lib/transaction';
 export { quote } from './lib/quote';
 export { sql } from './lib/sql';
 export { parseUrl } from './lib/parseUrl';
-export { Transaction, AdapterBase, AdapterProps, ResultMode, PgError };
+export { Transaction, AdapterBase, AdapterProps, ResultMode, PgError, Prepared };
 export declare class Adapter extends AdapterBase {
     static defaultLog: boolean | Log;
     connectionSettings: ConnectionSettingType;
@@ -17,5 +17,5 @@ export declare class Adapter extends AdapterBase {
     close: () => Promise<void>;
     transaction(fn?: (t: Transaction) => any): Promise<any[]>;
     wrapperTransaction(target: any, fn?: (t: typeof target & Transaction) => any): Promise<any[]>;
-    prepare(name: string, ...args: string[]): (prepareTemplate: TemplateStringsArray, prepareArgs?: any[] | undefined) => import("./types").Prepared;
+    prepare(name: string, ...args: string[]): (prepareTemplate: TemplateStringsArray, prepareArgs?: any[] | undefined) => Prepared;
 }
